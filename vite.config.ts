44957937e289343c406aa862e6f9cb7583e56f14
zainbinshakoor <yammybajwa09@gmail.com>
2024-11-06 19:47:37 +0500

    CD-1-Npm fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,35 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import ViteDts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    ViteDts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'CodeumUI',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `codeum-ui.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        format: 'es',
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+        exports: 'named',
+      },
+    },
+  },
   resolve: {
     alias: {
       '@components': '/src/components',
